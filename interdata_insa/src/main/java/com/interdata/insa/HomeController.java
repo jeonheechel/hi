cb4dab8241986b17b2f5addfe973a0ae99c5f2f8
jeonheechel <jeonheechel@DESKTOP-T29KS8P>
2020-03-18 11:10:41 +0900

    2020_03_18 전희철 예시 주석 커밋

--- interdata_insa/src/main/java/com/interdata/insa/HomeController.java
+++ interdata_insa/src/main/java/com/interdata/insa/HomeController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);// 예시 주석 ex) 전희철 2020_03_18 작성
 	
 	/**
 	 * Simply selects the home view to render by returning its name.
